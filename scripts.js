aa2106650e7333ac97a1fa09206fe356406ebdbe
Muhammad Arslan Wajid <167841484+marslanwajid@users.noreply.github.com>
2024-08-04 17:15:38 +0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -26,3 +26,25 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 });
+
+// Get the button
+const goTopBtn = document.getElementById("goTopBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    goTopBtn.style.display = "block";
+  } else {
+    goTopBtn.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+goTopBtn.addEventListener('click', function() {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+});
+
